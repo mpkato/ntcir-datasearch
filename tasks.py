217d0f95186f2740be4316d8aee7052f54d1c3bd
mpkato <kt.mako@gmail.com>
2020-02-08 22:41:46 +0900

    Stopped build multiple times

--- tasks.py
+++ tasks.py
@@ -10,7 +10,13 @@ def submodule(c):
 
 
 @task(submodule)
-def build(c):
+def build(c, recompile=False):
+    if os.path.exists("anserini/target/anserini-0.6.1-SNAPSHOT.jar")\
+          and not recompile:
+        print("Already compiled anserini. Stopped building it again.\n"
+              "Set --recompile option for enforcing the re-compilation.")
+        return
+
     cmd = "cd anserini && mvn clean package appassembler:assemble"
     result = c.run(cmd, pty=True)
     if result.ok:
